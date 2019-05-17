f46a530090d102c27c2e2dfd2e2dc01b435de551
devopsingh <devopsingh@gmail.com>
2019-05-17 09:33:57 +0100

    Initial Commit

---
+++ variables.tf
@@ -0,0 +1,26 @@
+#------------ variables.tf ----------------
+
+variable "aws_region" {}
+
+# Storage variables
+variable "project_name" {}
+
+# Networking variables
+variable "vpc_cidr" {}
+
+variable "public_cidrs" {
+  type = "list"
+}
+
+# Compute Variables
+variable "accessip" {}
+
+variable "key_name" {}
+
+variable "public_key_path" {}
+
+variable "server_instance_type" {}
+
+variable "instance_count" {
+  default = 1
+}
